2a39701adbbe1937e502317ea2066ef0d13567b0
Ben Swanson <benswanson127@gmail.com>
2017-05-30 16:15:12 -0600

    Create Module1_2.c

---
+++ Module1_2.c
@@ -0,0 +1,20 @@
+// Fig 2.5: fig02_05.c
+#include <stdio.h>
+
+// function main begins program execution
+int main(void)
+{
+	int integer1;
+	int integer2;
+
+	printf("Enter 1st int\n");
+	scanf("%d", &integer1);
+
+	printf("Enter 2nd int\n");
+	scanf("%d", &integer2);
+
+	int sum;
+
+	printf("Sum is %d\n", sum);
+
+} // end function main
