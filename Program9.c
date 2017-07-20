0b75c0f4ae76e0b8c67ff179d5ee1683543f831b
Ben Swanson <benswanson127@gmail.com>
2017-07-20 16:29:17 -0600

    Finished Program9

--- Program9.c
+++ Program9.c
@@ -42,15 +42,49 @@ int main(void) {
 	printf("Please enter the number to be converted into possible words. NOTE: Please avoid using 1s or 0s, and enter the phone number without a dash(-)");
 	scanf("%d", &phoneNumber);
 
+	//Empty Array for values
+	int *digit;
+	//Turn integer into array with each element as a digit
+	digit = toArray(phoneNumber);
+
+	//Empty chars
+	char *mword0, *mword1, *mword2, *mword3, *mword4, *mword5, *mword6;
+
+	for (level0 = 0; level0 < 3; level0++) {
+		mword0 = numberLetters[digit[0]][level0];
+		for (level1 = 0; level1 < 3; level1++) {
+			mword1 = numberLetters[digit[1]][level1];
+			for (level2 = 0; level2 < 3; level2++) {
+				mword2 = numberLetters[digit[2]][level2];
+				for (level3 = 0; level3 < 3; level3++) {
+					mword3 = numberLetters[digit[3]][level3];
+					for (level4 = 0; level4 < 3; level4++) {
+						mword4 = numberLetters[digit[4]][level4];
+						for (level5 = 0; level5 < 3; level5++) {
+							mword5 = numberLetters[digit[5]][level5];
+							for (level6 = 0; level6 < 3; level6++) {
+								mword6 = numberLetters[digit[6]][level6];
+								fprintf(fptr, "%c%c%c%c%c%c\n", mword0, mword1, mword2, mword3, mword4, mword5, mword6);
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+
+	fclose(fptr);
 
 }
 
-
-int *toArray(int number) {
+//Split number into array of 1 digit numbers
+int * toArray(int number)
+{
 	int n = log10(number) + 1;
 	int i;
-	char *numberArray = calloc(n, sizeof(char));
-	for (i = 0; i < n; ++i, number /= 10){
+	int *numberArray = calloc(n, sizeof(int));
+	for (i = 0; i < n; ++i, number /= 10)
+	{
 		numberArray[i] = number % 10;
 	}
 	return numberArray;
